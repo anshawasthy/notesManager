167e0cce187d0ae7e511b5de73a0453f83390a83
AnShAwAsThY10 <anshawasthy1234@gmail.com>
2026-05-17 19:39:05 +0530

    Update CORS configuration to allow requests from production URL

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookies());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://notes-manager.vercel.app',
     credentials: true
 }
 ));
